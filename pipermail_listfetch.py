93a562f545cfa939dd5910c6fccff8b7ea0f6dcf
Josef Friedrich <josef@friedrich.rocks>
2023-01-07 23:30:13 +0100

    December -> 12

--- pipermail_listfetch.py
+++ pipermail_listfetch.py
@@ -12,53 +12,82 @@ from urllib.parse import urlparse
 
 
 def main():
-
     ### Argument parsing ###
-    parser = argparse.ArgumentParser(description="Fetch mailing list archives.",
-        epilog="MIT license. (C) 2022 Alinur.\nFull license text in LICENSE file.")
-    parser.add_argument("webpage",
-        help="Pipermail webpage to parse and fetch mailing list archives from.")
-    parser.add_argument("--wait", type=float, metavar="s", default=1.5,
-        help="Interval between downloading each mailing list archive (in seconds)" +
-            "\nThis setting is for preventing overloading the server\n" +
-            "or triggering website rate limits. Default is 1.5.")
+    parser = argparse.ArgumentParser(
+        description="Fetch mailing list archives.",
+        epilog="MIT license. (C) 2022 Alinur.\nFull license text in LICENSE file.",
+    )
+    parser.add_argument(
+        "webpage",
+        help="Pipermail webpage to parse and fetch mailing list archives from.",
+    )
+    parser.add_argument(
+        "--wait",
+        type=float,
+        metavar="s",
+        default=1.5,
+        help="Interval between downloading each mailing list archive (in seconds)"
+        + "\nThis setting is for preventing overloading the server\n"
+        + "or triggering website rate limits. Default is 1.5.",
+    )
 
     args = parser.parse_args()
     interval = args.wait
 
     ### Webpage parsing ###
     page = requests.get(args.webpage).text
-    soup = bs.BeautifulSoup(page,'lxml')
+    soup = bs.BeautifulSoup(page, "lxml")
 
-    all_links=[]
-    for url in soup.find_all('a'):
-        all_links.append(url.get('href'))
+    all_links = []
+    for url in soup.find_all("a"):
+        all_links.append(url.get("href"))
 
-    archive_links=[]
+    archive_links = []
     for link in all_links:
-        link = link.replace(".gz",'')
+        link = link.replace(".gz", "")
         if link.endswith(".txt"):
             archive_links.append(link)
 
     ### Downloading ###
-    dled = 0
-    print(f"Downloaded {dled}/{str(len(archive_links))} mailing list archives\r",
-            end="")
+    downloaded = 0
+    print(
+        f"Downloaded {downloaded}/{str(len(archive_links))} mailing list archives\r",
+        end="",
+    )
     for archive in archive_links:
         ## Retrieve filename
-        fname = os.path.basename(urlparse(archive).path)
+        file_name = os.path.basename(urlparse(archive).path)
+
+        for mount, number in {
+            "January": "01",
+            "February": "02",
+            "March": "03",
+            "April": "04",
+            "May": "05",
+            "June": "06",
+            "July": "07",
+            "August": "08",
+            "September": "09",
+            "October": "10",
+            "November": "11",
+            "December": "12",
+        }.items():
+            file_name = file_name.replace(mount, number)
+
         ## Download archive
         archived_mail = requests.get(args.webpage + archive).text
 
         final_text = archived_mail
 
         ## Save archive
-        with open(fname, 'w') as archive_file:
+        with open(file_name, "w") as archive_file:
             archive_file.write(final_text)
 
-        dled += 1
-        print(f"Downloaded {dled}/{str(len(archive_links))} mailing list archives\r",
-                end="")
+        downloaded += 1
+        print(
+            f"Downloaded {downloaded}/{str(len(archive_links))} mailing list archives\r",
+            end="",
+        )
         sleep(interval)
 
     print(f"Finished downloading {str(len(archive_links))} mailing list archives.")
